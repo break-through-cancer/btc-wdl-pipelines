c964199707bedf2ec5f41e0500b98a30890d1cfe
Jennifer Chen <jennifer_a_chen@brown.edu>
2026-02-20 12:00:33 -0500

    try wiring

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -1,32 +1,112 @@
 import json
+import pandas as pd
 from cirro.helpers.preprocess_dataset import PreprocessDataset
-from cirro.api.models.s3_path import S3Path
 
-# def process_bams(ds):
-#     for base_name, group in ds.files.groupby("sample"):
-       
-#         for f in group["file"]:
-#             if f.endswith(".bam") and not f.endswith(".bam.bai"):
-#                 if "PBMC" in base_name:
-#                     normal_bam = f
-#                 else:
-#                     tumor_bam = f
-#             elif f.endswith(".bam.bai"):
-#                 if "PBMC" in base_name:
-#                     normal_bai = f
-#                 else:
-#                     tumor_bai = f
 
-#         if tumor_bam and tumor_bai:
-#             print(tumor_bai, tumor_bam)
-#         if normal_bam and normal_bai:
-#             print(normal_bai, normal_bam)
+def extract_bams(ds):
+    df = ds.files.copy()
+    df["file"] = df["file"].astype(str)
+
+    # Keep only BAM + BAI
+    df = df[df["file"].str.endswith(".bam") | df["file"].str.endswith(".bam.bai")]
+
+    bam_map = {}
+
+    for sample, group in df.groupby("sample"):
+        bam = ""
+        bai = ""
+
+        for f in group["file"]:
+            if f.endswith(".bam") and not f.endswith(".bam.bai"):
+                bam = f
+            elif f.endswith(".bam.bai"):
+                bai = f
+
+        if bam:
+            bam_map[str(sample)] = {"bam": bam, "bai": bai}
+
+    if not bam_map:
+        raise ValueError("No BAMs found in dataset")
+
+    return bam_map
+
 
 def main():
     ds = PreprocessDataset.from_running()
-    print(ds.files)
-    # process_bams(ds)
+
+    print("=== ds.files preview ===")
+    print(ds.files.head(20).to_string(index=False))
+
+    bam_map = extract_bams(ds)
+
+    tumor_bam = None
+    tumor_bai = None
+    normal_bam = None
+    normal_bai = None
+
+    # Simple rule: PBMC = normal, everything else = tumor
+    for sample, files in bam_map.items():
+        if "PBMC" in sample.upper():
+            normal_bam = files["bam"]
+            normal_bai = files["bai"]
+        else:
+            tumor_bam = files["bam"]
+            tumor_bai = files["bai"]
+
+    if not tumor_bam:
+        raise ValueError("No tumor BAM found")
+
+    # Always set tumor
+    ds.add_param("tumor_reads", tumor_bam)
+    ds.add_param("tumor_reads_index", tumor_bai)
+
+    # Only set normal if present
+    if normal_bam:
+        ds.add_param("normal_reads", normal_bam)
+        ds.add_param("normal_reads_index", normal_bai)
+        print("Matched normal detected.")
+    else:
+        print("No normal detected. Tumor-only mode.")
+
+    ds.save_parameters()
+
+    print("\nFinal parameters:")
+    print(json.dumps(ds.params, indent=2, default=str))
+
 
 if __name__ == "__main__":
     main()
+
+
+# import json
+# from cirro.helpers.preprocess_dataset import PreprocessDataset
+# from cirro.api.models.s3_path import S3Path
+
+# # def process_bams(ds):
+# #     for base_name, group in ds.files.groupby("sample"):
+       
+# #         for f in group["file"]:
+# #             if f.endswith(".bam") and not f.endswith(".bam.bai"):
+# #                 if "PBMC" in base_name:
+# #                     normal_bam = f
+# #                 else:
+# #                     tumor_bam = f
+# #             elif f.endswith(".bam.bai"):
+# #                 if "PBMC" in base_name:
+# #                     normal_bai = f
+# #                 else:
+# #                     tumor_bai = f
+
+# #         if tumor_bam and tumor_bai:
+# #             print(tumor_bai, tumor_bam)
+# #         if normal_bam and normal_bai:
+# #             print(normal_bai, normal_bam)
+
+# def main():
+#     ds = PreprocessDataset.from_running()
+#     print(ds.files)
+#     # process_bams(ds)
+
+# if __name__ == "__main__":
+#     main()
 
