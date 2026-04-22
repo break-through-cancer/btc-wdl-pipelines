3ba7ebdf3dfb477a0060ea26b2cf40b24930ac4c
Jennifer Chen <jennifer_a_chen@brown.edu>
2026-04-22 15:04:54 -0400

    incorrect preprocessingg

--- .cirro/preprocess.py
+++ .cirro/preprocess.py
@@ -1,12 +1,12 @@
-import cmd
 import json
 import pandas as pd
 from cirro.helpers.preprocess_dataset import PreprocessDataset
+
+
 def extract_bams(ds):
     df = ds.files.copy()
     df["file"] = df["file"].astype(str)
 
-    # Keep only BAM + BAI
     df = df[df["file"].str.endswith(".bam") | df["file"].str.endswith(".bam.bai")]
 
     bam_map = {}
@@ -29,7 +29,7 @@ def extract_bams(ds):
 
     return bam_map
 
-#for multiple mutect
+
 def main():
     ds = PreprocessDataset.from_running()
 
@@ -38,120 +38,43 @@ def main():
 
     bam_map = extract_bams(ds)
 
-    tumor_bam = None
-    tumor_bai = None
     normal_bam = None
     normal_bai = None
-    tumor_sample_name = None
+    tumor_runs = []
 
     for sample, files in bam_map.items():
-        if "PBMC" in sample.upper():
+        sample_name = str(sample)
+
+        if "PBMC" in sample_name.upper():
             normal_bam = files["bam"]
             normal_bai = files["bai"]
         else:
-            tumor_bam = files["bam"]
-            tumor_bai = files["bai"]
-            tumor_sample_name = str(sample)
+            tumor_runs.append({
+                "output_prefix": sample_name,
+                "tumor_reads": files["bam"],
+                "tumor_reads_index": files["bai"],
+                "tumor_sample_name": sample_name,
+            })
 
-    if not tumor_bam:
+    if not tumor_runs:
         raise ValueError("No tumor BAM found")
 
-    mutect_runs = [
-        {
-            "output_prefix": tumor_sample_name,
-            "tumor_reads": tumor_bam,
-            "tumor_reads_index": tumor_bai,
+    mutect_runs = []
+    for run in tumor_runs:
+        mutect_runs.append({
+            "output_prefix": run["output_prefix"],
+            "tumor_reads": run["tumor_reads"],
+            "tumor_reads_index": run["tumor_reads_index"],
             "normal_reads": normal_bam,
             "normal_reads_index": normal_bai,
-            "tumor_sample_name": tumor_sample_name,
-        }
-    ]
+            "tumor_sample_name": run["tumor_sample_name"],
+        })
 
     ds.add_param("mutect_runs", mutect_runs)
 
     print("\nFinal parameters:")
     print(json.dumps(ds.params, indent=2, default=str))
 
-#for working non multiple mutect
-
-# def main():
-#     ds = PreprocessDataset.from_running()
-
-#     print("=== ds.files preview ===")
-#     print(ds.files.head(20).to_string(index=False))
-
-#     bam_path = ds.files[ds.files['file'].str.endswith('.bam')]['file'].iloc[0]
-
-#     bam_map = extract_bams(ds)
-
-#     tumor_bam = None
-#     tumor_bai = None
-#     normal_bam = None
-#     normal_bai = None
-
-#     # Simple rule: PBMC = normal, everything else = tumor
-#     for sample, files in bam_map.items():
-#         if "PBMC" in sample.upper(): # switch on the sample type column "Status"
-#             normal_bam = files["bam"]
-#             normal_bai = files["bai"]
-#         else:
-#             tumor_bam = files["bam"]
-#             tumor_bai = files["bai"]
-#             tumor_sample_name = str(sample)
-
-#     if not tumor_bam:
-#         raise ValueError("No tumor BAM found")
-
-#     # Always set tumor
-#     ds.add_param("tumor_reads", tumor_bam)
-#     ds.add_param("tumor_reads_index", tumor_bai)
-#     ds.add_param("tumor_sample_name", tumor_sample_name)
-
-#     # Only set normal if present
-#     if normal_bam:
-#         ds.add_param("normal_reads", normal_bam)
-#         ds.add_param("normal_reads_index", normal_bai)
-#         print("Matched normal detected.")
-#     else:
-#         print("No normal detected. Tumor-only mode.")
-
-#     print("\nFinal parameters:")
-#     print(json.dumps(ds.params, indent=2, default=str))
-
 
 if __name__ == "__main__":
     main()
-
-
-# import json
-# from cirro.helpers.preprocess_dataset import PreprocessDataset
-# from cirro.api.models.s3_path import S3Path
-
-# # def process_bams(ds):
-# #     for base_name, group in ds.files.groupby("sample"):
-       
-# #         for f in group["file"]:
-# #             if f.endswith(".bam") and not f.endswith(".bam.bai"):
-# #                 if "PBMC" in base_name:
-# #                     normal_bam = f
-# #                 else:
-# #                     tumor_bam = f
-# #             elif f.endswith(".bam.bai"):
-# #                 if "PBMC" in base_name:
-# #                     normal_bai = f
-# #                 else:
-# #                     tumor_bai = f
-
-# #         if tumor_bam and tumor_bai:
-# #             print(tumor_bai, tumor_bam)
-# #         if normal_bam and normal_bai:
-# #             print(normal_bai, normal_bam)
-
-# def main():
-#     ds = PreprocessDataset.from_running()
-#     print(ds.files)
-#     # process_bams(ds)
-
-# if __name__ == "__main__":
-#     main()
-
